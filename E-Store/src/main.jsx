60dee0258b8048b846181500beb6b62103740a55
Bodhisattwa Basu <basubodhisattwa6@gmail.com>
2023-06-22 13:05:08 +0530

    changes 4

--- E-Store/src/main.jsx
+++ E-Store/src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <BrowserRouter>
+      <BrowserRouter basename="/index.html">
         <App />
       </BrowserRouter>
   </React.StrictMode>,
