e1aafe885bcaf97e5b596f2daaba107da01558ef
Bodhisattwa Basu <basubodhisattwa6@gmail.com>
2023-06-22 17:03:19 +0530

    changes cors new

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ const stripe = require('stripe')('sk_test_51MTeHYSFe9iNERqeQmzflPrsqoCAml9j7hfYy
 const salt = bcrypt.genSaltSync(10);
 const secret = 'asdfe45we45w345wegw345werjktjwertkj';
 
-app.use(cors({ credentials: true, origin: ['http://localhost:5173','https://shop-mop-2agc.onrender.com/'] }));
+app.use(cors({ credentials: true, origin: ['http://localhost:5173','https://shop-mop-back.onrender.com','https://shop-mop.vercel.app/'] }));
 app.use(express.json());
 app.use(cookieParser());
 // cors("*")
