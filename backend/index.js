ccbbcff9599e75acec6bd2867c5065cb8c93dad0
Bodhisattwa Basu <68936778+Bodhi132@users.noreply.github.com>
2023-10-08 15:07:25 +0530

    Backend Code Cleanup

--- backend/index.js
+++ backend/index.js
@@ -9,21 +9,15 @@ const app = express();
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 const multer = require('multer');
-// const uploadUserMiddleware = multer({ dest: 'userUploads/' });
 const fs = require('fs');
 const bodyparser = require('body-parser');
 const stripe = require('stripe')('sk_test_51MTeHYSFe9iNERqeQmzflPrsqoCAml9j7hfYymqmNwmVbRCAyieAhqZTNone27sdTIIYe2sQy50vqQtcOQSl67uG00WsZ8Znhq')
-// const { log } = require("console");
-//sk_test_51MTeHYSFe9iNERqeQmzflPrsqoCAml9j7hfYymqmNwmVbRCAyieAhqZTNone27sdTIIYe2sQy50vqQtcOQSl67uG00WsZ8Znhq
 const salt = bcrypt.genSaltSync(10);
 const secret = 'asdfe45we45w345wegw345werjktjwertkj';
 
 app.use(cors({ credentials: true, origin: ['http://localhost:5173','https://shop-mop-static.onrender.com'] }));
 app.use(express.json());
 app.use(cookieParser());
-// cors("*")
-// app.use(express.static(__dirname))
-// app.use('/uploads', express.static(__dirname + '/uploads'));
 
 const userStorage = multer.diskStorage({
     destination: (req, file, callback) => {
@@ -42,36 +36,7 @@ mongoose.connect(uri);
 
 app.post('/signup', uploadUserMiddleware.single('file'), (req, res) => {
 
-    // const { originalname, path } = req.file;
-    // const parts = originalname.split('.');
-    // const ext = parts[parts.length - 1];
-    // const newPath = path + '.' + ext;
-    // fs.renameSync(path, newPath);
-    // console.log(newPath);
-
-    // console.log(req.file);
-
-
-
-    // try {
-    //     const newUserData = await User.create({
-    //         username,
-    //         email,
-    //         password: bcrypt.hashSync(password, bcryptSalt),
-    //         photo,
-    //         photoPath:newPath
-    //     })
-    //     res.json(newUserData)
-    // } catch (e) {
-    //     res.status(422).json(e)
-    // }
-    // const userExists =  User.findOne( req.body.email );
-
-    // if (userExists) {
-    //   res.status(404);
-    //   throw new Error("Email has already been registered");
-    // }
-
+    
     const newUser = new User({
 
         username: req.body.username,
@@ -122,54 +87,6 @@ app.get('/profile', (req, res) => {
         res.json(null);
     }
 });
-// app.post('/cart', (req, res) => {
-//     const { token } = req.cookies;
-//     const { details, count } = req.body
-
-//     jwt.verify(token, secret, {}, async (err, userData) => {
-//         if (err) throw err;
-//         // res.json(details);
-
-//         // const cart = await Cart.create({
-//         //     image: details[0].images[0],
-//         //     item: details[0].title,
-//         //     price: details[0].price,
-//         //     quantity: count,
-//         //     subtotal: details[0].price * count,
-//         //     purchaser: userData.id
-//         // })
-//         // res.json(cart)
-//         if (token) res.json('present')
-//     });
-
-// });
-
-// app.post('/checkout',async(req,res)=>{
-
-//     console.log(req.body);
-//     const items=req.body.items
-//     let lineItems = []
-//     items.forEach((item)=>{
-//         lineItems.push(
-//             {
-//                 price:item.id,
-//                 quantity:item.quantity
-//             }
-//         )
-//     })
-
-//     const session = await stripe.checkout.sessions.create({
-//         line_items:lineItems,
-//         mode:'payment',
-//         success_url:'http://localhost:5173/success',
-//         cancel_url:'http://localhost:5173/cancel',
-//     })
-
-//     res.send(JSON.stringify({
-//         url:session.url
-//     }))
-
-// })
 
 app.post("/create-checkout-session", async (req, res)=>{
     try{
